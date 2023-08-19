11a26a79654a555cc767e81d2ed83f021269a15b
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-08-18 22:22:21 -0300

    in

--- MyFrame/estrutura/Class_Campo.php
+++ MyFrame/estrutura/Class_Campo.php
@@ -5,7 +5,77 @@ namespace estrutura;
 /**
  * classe de campos
  */
- class Campo {
-     
- }
+class CampoNumerico
+{
+    private $nome;
+    private $id;
+    private $label;
 
+    public function __construct(string $nome, string $id, string $label)
+    {
+        $this->setNome($nome);
+        $this->setId($id);
+        $this->setLabel($label);
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id 
+     * @return self
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome 
+     * @return self
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param mixed $label 
+     * @return self
+     */
+    public function setLabel($label): self
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    public function hide()
+    {
+    }
+}
