211a8b0553a69997785fbf9dc5fd9d134822af3b
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2023-08-19 11:03:21 -0300

    Retirar métodos depreciados

--- MyFrame/estrutura/js/js_principal.js
+++ MyFrame/estrutura/js/js_principal.js
@@ -4,26 +4,5 @@ $(function () {
         $(this).css('grid-column', iCards);
         iCards += 2;
     });
-    // var maiorLinha = 0;
-    // $(".container .linha").each(function () {
-    //     var larguraLinha = 0;
-    //     $(this).children(".tile").each(function () {
-    //         larguraLinha += $(this).width();
-    //         larguraLinha += 2 * parseInt($(this).css("margin-right").toString().replace("px", ""));
-    //     });
-    //     if (larguraLinha > maiorLinha)
-    //         maiorLinha = larguraLinha + 5;
-    // });
-    // $(".container").css("width", maiorLinha.toString() + "px");
-});
-$(".tile").mousedown(function () {
-    $(this).addClass("selecionado");
-});
-
-$(".tile").mouseup(function () {
-    $(this).removeClass("selecionado");
-});
-$(".tile").mouseleave(function () {
-    $(this).removeClass("selecionado");
 });
 
