202fda2813f822ef4e4798fc1ab73313533b7b6b
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-08-19 10:56:35 -0300

    in

--- MyFrame/estrutura/class_campo_numerico.php
+++ MyFrame/estrutura/class_campo_numerico.php
@@ -2,82 +2,24 @@
 
 namespace estrutura;
 
+use estrutura\Campo;
+
 /**
  * classe de campos
  * @author Otavio Prada
  * @since 19/08/2023
  */
-class CampoNumerico
+class CampoNumerico extends Campo
 {
-    public $nome;
-    public $id;
-    public $label;
+    
 
-    public function __construct(string $nome, string $id, string $label)
+    public function __construct(string $nome, string $label, int $tamanho)
     {
         $this->setNome($nome);
-        $this->setId($id);
+        $this->setId($nome);
         $this->setLabel($label);
+        $this->setTamanho($tamanho);
 
         return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id 
-     * @return self
-     */
-    public function setId($id): self
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getNome()
-    {
-        return $this->nome;
-    }
-
-    /**
-     * @param mixed $nome 
-     * @return self
-     */
-    public function setNome($nome): self
-    {
-        $this->nome = $nome;
-        return $this;
-    }
-
-
-    /**
-     * @return mixed
-     */
-    public function getLabel()
-    {
-        return $this->label;
-    }
-
-    /**
-     * @param mixed $label 
-     * @return self
-     */
-    public function setLabel($label): self
-    {
-        $this->label = $label;
-        return $this;
-    }
-
-    public function hide()
-    {
     }
 }
