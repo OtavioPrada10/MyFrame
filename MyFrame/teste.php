a1b148edc659c0d3dbd90157e5c89a02c579d38a
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-08-18 21:50:27 -0300

    teste

--- MyFrame/teste.php
+++ MyFrame/teste.php
@@ -9,12 +9,5 @@ $dbname = "myframedb";
 $username = "root";
 $password = "";
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username,
-$password);
-    echo "Conectado a $dbname em $host com sucesso.";
-} catch (PDOException $pe) {
-    die("Não foi possível se conectar ao banco de dados $dbname :" . $pe
->getMessage());
+
     
-}
