407f5df1df65c571293dd61a958916f24358169a
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2023-08-20 15:02:08 -0300

    Início das animações do card

--- MyFrame/index.php
+++ MyFrame/index.php
@@ -30,13 +30,13 @@
     <section>
         <div class="card-area">
             <span class="card-previous">
-                <span class="fa-solid fa-circle-chevron-right fa-flip-horizontal fa-4x" style="color: #ffffff;"></span>
+                <span class="fa-solid fa-circle-chevron-right fa-flip-horizontal fa-4x card-icon"></span>
             </span>
             <a href="include/view/view_consulta_Contas.php" class="card-link">
                 <div class="card">
                     <span class="card-content">
                         <span class="card-icon-area">
-                            <span class="fa-solid fa-sack-dollar fa-3x" style="color: #ffffff;"></span>
+                            <span class="fa-solid fa-sack-dollar fa-3x card-icon"></span>
                         </span>
                         <span class="card-text">Consultar Contas</span><br>
                     </span>
@@ -46,7 +46,7 @@
                 <div class="card">
                     <span class="card-content">
                         <span class="card-icon-area">
-                            <span class="fa-solid fa-star fa-3x" style="color: #ffffff;"></span>
+                            <span class="fa-solid fa-star fa-3x card-icon"></span>
                         </span>
                         <span class="card-text">Favoritos</span>
                     </span>
@@ -56,7 +56,7 @@
                 <div class="card">
                     <span class="card-content">
                         <span class="card-icon-area">
-                            <span class="fa-solid fa-magnifying-glass-dollar fa-3x" style="color: #ffffff;"></span>
+                            <span class="fa-solid fa-magnifying-glass-dollar fa-3x card-icon"></span>
                         </span>
                         <span class="card-text">Navegador</span>
                     </span>
@@ -66,7 +66,7 @@
                 <div class="card">
                     <span class="card-content">
                         <span class="card-icon-area">
-                            <span class="fa-solid fa-image fa-3x" style="color: #ffffff;"></span>
+                            <span class="fa-solid fa-image fa-3x card-icon"></span>
                         </span>
                         <span class="card-text">Imagens</span>
                     </span>
@@ -76,7 +76,7 @@
                 <div class="card">
                     <span class="card-content">
                         <span class="card-icon-area">
-                            <span class="fa-solid fa-file fa-3x" style="color: #ffffff;"></span>
+                            <span class="fa-solid fa-file fa-3x card-icon"></span>
                         </span>
                         <span class="card-text">Arquivos</span>
                     </span>
@@ -86,14 +86,14 @@
                 <div class="card">
                     <span class="card-content">
                         <span class="card-icon-area">
-                            <span class="fa-solid fa-question fa-3x" style="color: #ffffff;"></span>
+                            <span class="fa-solid fa-question fa-3x card-icon"></span>
                         </span>
                         <span class="card-text">Ajuda</span>
                     </span>
                 </div>
             </a>
             <span class="card-next">
-                <span class="fa-solid fa-circle-chevron-right fa-4x" style="color: #ffffff;"></span>
+                <span class="fa-solid fa-circle-chevron-right fa-4x card-icon"></span>
             </span>
         </div>
     </section>
