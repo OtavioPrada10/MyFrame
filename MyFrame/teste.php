e9e99273147eb5d2ffd5d705c61b3ab0a9305cec
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-08-21 11:58:09 -0300

    in

--- MyFrame/teste.php
+++ MyFrame/teste.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * site com tutorial
- * https://www.devmedia.com.br/crud-com-php-pdo/28873
- */
 
-$host = 'localhost';
-$dbname = "myframedb";
-$username = "root";
-$password = "";
+$teste = 10;
 
+
+
+var_dump($_SERVER);
