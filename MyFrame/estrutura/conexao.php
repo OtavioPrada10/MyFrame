4b0906e1cec5e81f51c3d977c5c572b4d3ebb9e5
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-08-21 11:13:40 -0300

    in

--- MyFrame/estrutura/conexao.php
+++ MyFrame/estrutura/conexao.php
@@ -24,15 +24,12 @@ $options    = array(
   );
 
 try {
-    // sgbd:host;port;dbname
-    // usuario
-    // senha
-    // errmode
     $pdo = new PDO("pgsql:host=$host;port=$porta;dbname=$database", $user, $password, $options);
 
-    //echo "Conectado no banco de dados!!!";
+    echo "Conectado no banco de dados!!!";
 
 } catch (PDOException $e) {
     echo "Falha ao conectar ao banco de dados. <br/>";
     die($e->getMessage());
 }
+
