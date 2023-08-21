42a28974deed2d786d7dfc1657dd2467ce86ded4
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-08-21 12:54:08 -0300

    teste

--- MyFrame/estrutura/conexao.php
+++ MyFrame/estrutura/conexao.php
@@ -1,12 +1,10 @@
 <?php
-$host = "containers-us-west-51.railway.app";
+$host     = "containers-us-west-51.railway.app";
 $database = "railway";
-$user = "postgres";
+$user     = "postgres";
 $password = "gcuXnwMXebavNlVGcsfw";
-$porta = 5840;
-$options    = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  );
+$porta    = 5840;
+$options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$porta;dbname=$database", $user, $password, $options);
@@ -16,4 +14,4 @@ try {
 } catch (PDOException $e) {
     echo "Falha ao conectar ao banco de dados. <br/>";
     die($e->getMessage());
-}
+} 
